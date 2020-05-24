26a363ad0f1def2ac7b50992f47f1f24e12c5784
lanaumch <lanaumch@gmail.com>
2020-05-25 00:05:57 +0300

    add location tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(WeatherApp());
 
 class WeatherApp extends StatelessWidget {
-  static const String _title = 'WeatherApp';
+  static const String _title = 'Weather App';
 
   @override
   Widget build(BuildContext context) {
@@ -23,23 +25,70 @@ class WeatherStatefulWidget extends StatefulWidget {
 
 class _WeatherStatefulWidgetState extends State<WeatherStatefulWidget> {
   int _selectedIndex = 0;
+  Geolocator geolocator = Geolocator();
+  static Position userLocation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getLocation().then((position) {
+      userLocation = position;
+    });
+  }
+
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Today',
+  static List<Widget> _widgetOptions = <Widget>[
+    userLocation == null
+        ? CircularProgressIndicator()
+        : Text("Location:" +
+          userLocation.latitude.toString() +
+          " " +
+          userLocation.longitude.toString(),
       style: optionStyle,
     ),
-    Text(
-      'Forecast',
-      style: optionStyle,
+    ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.wb_sunny),
+          title: Text('row $index'),
+          subtitle: Text('Sunny'),
+          trailing: Text('22°'),
+        );
+      }
     ),
   ];
 
   void _onItemTapped(int index) {
+    _getLocation().then((value) {
+      setState(() {
+        userLocation = value;
+        print("VALUE: "+userLocation.latitude.toString() );
+      });
+      _widgetOptions[0]= userLocation == null
+          ? CircularProgressIndicator()
+          : Text("Location:" +
+          userLocation.latitude.toString() +
+          " " +
+          userLocation.longitude.toString(),
+        style: optionStyle,
+      );
+    });
     setState(() {
       _selectedIndex = index;
+      print('INDEX');
     });
+  }
+  Future<Position> _getLocation() async {
+    var currentLocation;
+    try {
+      currentLocation = await geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
   }
 
 
@@ -48,7 +97,7 @@ class _WeatherStatefulWidgetState extends State<WeatherStatefulWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Weather application'),
+        title: const Text('Weather app'),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
@@ -56,8 +105,8 @@ class _WeatherStatefulWidgetState extends State<WeatherStatefulWidget> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.wb_sunny),
-            title: Text('Today')
+              icon: Icon(Icons.wb_sunny),
+              title: Text('Today')
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.wb_cloudy),
